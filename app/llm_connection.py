6fac32a2926f2b9e140e2397bdb2c9f9bf7b7b83
Ansh <aharjai21@gmail.com>
2025-03-07 14:35:11 -0500

    Update llm_connection.py

--- app/llm_connection.py
+++ app/llm_connection.py
@@ -1,6 +1,6 @@
 from together import Together
 
-together_ai_api = "21003311b33c7dddadb89d6c0c7b42e81d8e39c866f672692b15a404e32351e6"
+# together_ai_api = "Enter you API key here"
 
 def create_prompt(query, context):
     sys_prompt = "You are an AI assistant for a robotics company. Your task is to answer questions about ROS2, Nav2, MoveIt2, and Gazebo. Use the provided data to generate responses for the given question. Your responses should be informative and accurate."
